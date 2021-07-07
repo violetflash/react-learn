d08eeece64175d43f8c483cadf69e8ad66387add
Alex Grobovsky <ktotaam@mail.ru>
2021-07-08 01:25:23 +0500

    save

---
+++ Mueller/03.Styling/cssModules/src/Components/Person/Person.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Person = props => {
+
+    const style = {
+        display: 'block',
+        '@media (min-width: 500px)': {
+            backgroundColor: 'palegreen',
+            padding: '15px'
+        }
+    };
+
+    return (
+        <>
+           <span onClick={props.click} style={style}>
+                {props.name}, {props.age}
+            </span>
+            <input type="text" onChange={props.change}/>
+        </>
+
+    )
+
+};
+
+export default Person;
