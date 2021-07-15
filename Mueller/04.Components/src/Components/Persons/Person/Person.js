213a8798db5e240ef59a22157a3f004e488b3e67
Alex Grobovsky <ktotaam@mail.ru>
2021-07-15 11:50:21 +0500

    save

--- Mueller/04.Components/src/Components/Persons/Person/Person.js
+++ Mueller/04.Components/src/Components/Persons/Person/Person.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import classes from './Person.module.scss';
 
-const Person = props => {
+class Person extends React.Component {
+    render() {
 
-    console.log('[Person.js] rendering...')
-    // const rnd = Math.random();
-    // if (rnd > 0.7) {
-    //     throw new Error('Something went wrong');
-    // }
+        console.log('[Person.js] rendering...');
+        // const rnd = Math.random();
+        // if (rnd > 0.7) {
+        //     throw new Error('Something went wrong');
+        // }
 
-    return (
-        <div className={classes.Person}>
-           <span  onClick={props.click} >
-                {props.name}, {props.age}
+        return (
+            <div className={classes.Person}>
+           <span onClick={this.props.click}>
+                {this.props.name}, {this.props.age}
             </span>
-            <input type="text" onChange={props.change}/>
-            <button className={classes.Button}>example</button>
-        </div>
+                <input type="text"
+                       onChange={this.props.change}
+                       value={this.props.name}
+                />
+                <button className={classes.Button}>example</button>
+            </div>
 
-    )
+        );
 
-};
+    }
+}
 
 export default Person;
--- Mueller/04.Components/src/Components/Persons/Persons.js
+++ Mueller/04.Components/src/Components/Persons/Persons.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import Person from "./Person/Person";
 
-const Persons = props => {
-    console.log('[PersonS.js] rendering...')
-    return (
-        props.persons.map((person, index) => (
-            <Person
-                key={person.id}
-                name={person.name}
-                age={person.age}
-                click={() => props.clicked(index)}
-                change={(e) => props.changed(e, person.id)}
-            />
-        ))
-    );
+class Persons extends React.Component {
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDerivedStateFromProps');
+    //     return state;
+    // }
 
-};
+    shouldComponentUpdate(nextProps, nextState) {
+        //compare current props with next props and return true or false to render or not
+        console.log('[Persons.js] shouldComponentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate');
+    }
+
+    render() {
+        console.log('[PersonS.js] rendering...');
+        return (
+            this.props.persons.map((person, index) => (
+                <Person
+                    key={person.id}
+                    name={person.name}
+                    age={person.age}
+                    click={() => this.props.clicked(index)}
+                    change={(e) => this.props.changed(e, person.id)}
+                />
+            ))
+        );
+
+    }
+}
 
 export default Persons;
